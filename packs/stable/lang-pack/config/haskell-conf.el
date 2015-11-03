69e932ac2f94da3786d990e5ee1c89c46221f666
Mikhail Selivanov <selivanov.mike@gmail.com>
2015-11-03 19:47:56 +0000

    Fix save handler for Haskell files
    When try to save buffer to a file the following error occurs:
    
    run-hooks: Symbol's function definition is void: haskell-mode-after-save-handler

--- packs/stable/lang-pack/config/haskell-conf.el
+++ packs/stable/lang-pack/config/haskell-conf.el
@@ -2,6 +2,7 @@
 (require 'haskell-font-lock)
 (require 'haskell-simple-indent)
 (require 'haskell-mode)
+(require 'haskell)
 
 
 (add-to-list 'auto-mode-alist        '("\\.\\(?:[gh]s\\|hi\\)\\'" . haskell-mode))
